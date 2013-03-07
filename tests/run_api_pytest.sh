aee67e5e2cc6639f2328c6235fe62090eacddd0c
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2013-03-07 15:05:32 +0100

    do not delete tests/output directory at the beginning of each script

--- tests/run_api_pytest.sh
+++ tests/run_api_pytest.sh
@@ -3,9 +3,6 @@
 # setup env : go to the root of the project and activate stuff
 cd "$(dirname $0)/../"
 . ./setup/activate
-
-# cleanup output directory
-rm -rf ./tests/output
 mkdir -p ./tests/output
 
 py.test --junitxml tests/output/api_pytest.xml tests/api_pytest/api.py
